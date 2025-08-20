48194feb1281a64edbb7f275d167b5c23cc194d2
vincentj87 <vincentjonathan870@gmail.com>
2025-08-20 17:06:47 +0700

    mature program

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 // === Ethernet Settings ===
+#define ETH_RST 25 // Ethernet Reset Pin
 #define ETH_SPI_SCS 5 // W5500 CS
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEF};
 // === ONE SEGMENT WITH SERVER CONFIG IP ===
@@ -18,7 +19,7 @@ byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEF};
 // IPAddress mqttServer(172,17,210,59);
 
 // === ONE SEGMENT WITH SWITCH CONFIG IP ===
-IPAddress ip(172, 19, 16, 65);
+IPAddress ip(172, 19, 16, 61);
 IPAddress gateway(172, 19, 17, 254);
 IPAddress subnet(255, 255, 254, 0);
 IPAddress mqttServer(172, 19, 16, 243);
@@ -27,10 +28,11 @@ EthernetClient ethClient;
 #include "UNIT_UHF_RFID.h"
 HardwareSerial SerialRFID(1);
 Unit_UHF_RFID uhf;
-String rfidReadResult = "TEST123";
+String rfidReadResult = "";
 // === Bluetooth Settings ===
 #include <BluetoothSerial.h>
 BluetoothSerial SerialBT;
+String barcodeReadBuffer="";
 bool isBarcodeConnected = false, prevBarcodeConnected = false;
 String barcodeReadResult = "";
 // aa:a8:02:05:55:bd kassen mac
@@ -40,6 +42,8 @@ unsigned long lastReadRFID = 0;
 // === MQTT Settings ===
 #define MQTT_Port 1883
 #define MQTT_HearbeatDuration 1000
+#define telemetryTopic "mkm/up/pairing_station/telemetry" // MQTT topic for sending data
+#define heartbeatTopic "mkm/up/pairing_station/heartbeat" // MQTT topic for heartbeat
 void callback(char *topic, byte *payload, unsigned int length);
 PubSubClient mqttClient(mqttServer, MQTT_Port, callback, ethClient);
 const char *mqtt_username = "admin";
@@ -76,104 +80,69 @@ void barcodeMantain()
 
   if (!SerialBT.connected())
   {
-    prevBarcodeConnected = false;
-    tft.setTextSize(2);
-    Serial.println("Attempting to connect to Zebra scanner...");
-    SerialBT.connect(kassen_mac);
-    Serial.println("Failed to connect. Retrying in 5 seconds...");
-    tft.fillRect(0, 40, 320, 16, ILI9341_BLACK);
-    tft.setCursor(10, 40);
-    tft.setTextColor(ILI9341_RED);
-
-    tft.print("KASSEN : DISCONNECTED");
-  }
-  else
-  {
-    if (prevBarcodeConnected == false)
+    Serial.println("barcodeMaintain(): Bluetooth not connected. Attempting to reconnect...");
+    if (SerialBT.connect(kassen_mac))
     {
-
-      tft.setTextSize(2);
-      Serial.println("Connected to scanner!");
-      tft.fillRect(0, 40, 320, 16, ILI9341_BLACK);
-      tft.setCursor(10, 40);
-      tft.setTextColor(ILI9341_GREEN);
-      tft.print("KASSEN : CONNECTED");
-      prevBarcodeConnected = true;
+      Serial.println(" barcodeMaintain(): Bluetooth reconnected successfully!");
+    }
+    else
+    {
+      Serial.println(" barcodeMaintain(): Failed to reconnect Bluetooth.");
+      delay(1000); // Wait before retrying
     }
   }
+  
 }
 
 void barcodeBegin()
 {
 
-  SerialBT.begin("MKM_Pairing_Station", true);
+  SerialBT.begin("MKM-Pairing-Station", true);
   Serial.println("Bluetooth initialized");
 
-  Serial.println("Waiting for Zebra scanner connection...");
-  tft.fillRect(0, 40, 320, 16, ILI9341_BLACK);
-  tft.setCursor(10, 40);
-  tft.setTextColor(ILI9341_RED);
-  tft.print("KASSEN : CONNECTING");
-  SerialBT.connect(kassen_mac);
-
 }
-void barcodeShowTFT()
-{
-  // Clear below text lines instead
-  tft.fillRect(0, 190, 320, 40, ILI9341_BLACK); // 240 wide, 40px tall (160 to 200)
-  tft.setCursor(10, 190);
-  tft.setTextColor(ILI9341_GREEN);
-  tft.print("BRCD: ");
-  tft.setCursor(90, 190);
+void tftShowBarcode(){
+// print barcode read result
   tft.setTextColor(ILI9341_WHITE);
-  // tft.print(uhf.cards[0].epc_str);
+  tft.fillRect(0,130,320,30,ILI9341_BLACK);
+  tft.setCursor(10, 130);
+  tft.print("BRCD");
+  tft.setCursor(90,130);
   tft.print(barcodeReadResult);
+
+  
+}
+void tftShowRfid(){
+  // print rfid read result 
+  tft.setTextColor(ILI9341_WHITE);
+  tft.fillRect(0,160,320,30,ILI9341_BLACK);
+  tft.setCursor(10, 160);
+  tft.print("RFID");
+  tft.fillRect(0,190,320,30,ILI9341_BLACK);
+  tft.setCursor(10,190);
+  tft.print(rfidReadResult);
 }
 void barcodeRead()
 {
-  if ((millis() - nowT) > 100)
-  {
-    if (SerialBT.available())
-    {
-      barcodeReadResult = "";
-      while (SerialBT.available() > 0)
-      {
-        char c = SerialBT.read();
-        if (c == '\n' || c == '\r')
-        {
-          // End of barcode
-          break;
-        }
-        barcodeReadResult += c;
-      }
-      barcodeReadResult.trim(); // Remove any leading/trailing whitespace
-      Serial.println("Barcode: " + barcodeReadResult);
-      barcodeShowTFT();
-      // mqttClient.publish("barcode", barcode.c_str());
-      nowT = millis();
-    }
+  while (SerialBT.available()){
+    char c = SerialBT.read();
+    if(c!='\r'){
+      barcodeReadBuffer+=c;
+    } 
+    
   }
+  if(barcodeReadBuffer.length()>0){
+    Serial.println("Barcode Read: " + barcodeReadBuffer);
+    barcodeReadResult = barcodeReadBuffer;
+    barcodeReadBuffer="";
+    tftShowBarcode();
+  }    
 }
-void rfidShowTFT()
-{
-  // Clear below text lines instead
-  tft.fillRect(0, 160, 320, 30, ILI9341_BLACK); // 240 wide, 40px tall (160 to 200)
-  tft.setCursor(10, 160);
-  tft.setTextColor(ILI9341_GREEN);
-  tft.print("RFID: ");
-  tft.setCursor(90, 160);
-  tft.setTextColor(ILI9341_WHITE);
-  // tft.print(uhf.cards[0].epc_str);
-  tft.print(rfidReadResult);
-}
+
 void rfidRead()
 {
-  if ((millis() - lastReadRFID) > 100)
-  {
-    rfidShowTFT();
+
     uint8_t result = uhf.pollingMultiple(20);
-    // for test 
-    rfidReadResult="TEST";
     if (result > 0)
     {
       for (uint8_t i = 0; i < result; i++)
@@ -183,26 +152,12 @@ void rfidRead()
         Serial.println("epc: " + uhf.cards[i].epc_str);
         Serial.println("-----------------");
         rfidReadResult = uhf.cards[i].epc_str;
+        tftShowRfid();
       }
-      //  rfidShowTFT();
     }
-
-    // String barcode="";
-
-    // while(SerialRFID.available()){
-    //  uint8_t c= SerialRFID.read();
-
-    // }
-    lastReadRFID = millis();
-  }
 }
-void dataUpdate()
-{
-  String rfidData = "";
-  String barcodeData = "";
-  rfidData = uhf.cards[0].epc_str;
-  barcodeData = "";
-}
+
+
 
 void rfidSetup()
 {
@@ -234,22 +189,13 @@ void connectToMQTT()
     if (mqttClient.connect("ESP32Client", mqtt_username, mqtt_password))
     {
       Serial.println("connected!");
-      tft.fillRect(0, 100, 320, 20, ILI9341_BLACK);
-      tft.setCursor(10, 100);
-      tft.setTextColor(ILI9341_GREEN);
-      tft.print("MQTT: OK");
       mqttClient.subscribe("test/topic");
     }
     else
     {
-      tft.fillRect(0, 100, 320, 20, ILI9341_BLACK);
-      tft.setCursor(10, 100);
-      tft.setTextColor(ILI9341_RED);
-      tft.print("MQTT: DISCONNECTED");
       Serial.print("failed, rc=");
       Serial.print(mqttClient.state());
-      Serial.println(" try again in 3 seconds");
-      
+      Serial.println(" try again in 3 seconds"); 
       delay(3000);
     }
   }
@@ -260,7 +206,7 @@ void heartBeatRoutine()
   {
     MQTT_nowT = millis();
 
-    mqttClient.publish("mkm/up/scanner/rfid/heartbeat", "{\"heartbeat\":1}");
+    mqttClient.publish(heartbeatTopic, "{\"heartbeat\":1}");
   }
 }
 void HomePage()
@@ -285,27 +231,50 @@ void buttonInit()
   pinMode(BUTTON1_PIN, INPUT); // Using external pull-up
   pinMode(BUTTON2_PIN, INPUT);
 }
-void mqttStatusTFT(){
-  if(!mqttClient.connected()){
-    tft.fillRect(0, 130, 320, 20, ILI9341_BLACK);
-      tft.setCursor(10, 130);
-      tft.setTextColor(ILI9341_RED);
-      tft.print("MQTT: NOT CONNECTED");
-  }else{
-    tft.fillRect(0, 130, 320, 20, ILI9341_BLACK);
-      tft.setCursor(10, 130);
-      tft.setTextColor(ILI9341_GREEN);
-      tft.print("MQTT: CONNECTED");
+void ConnectivityStatusTFT(){
+  // bluetooth status
+  if(!SerialBT.connected())
+  {
+    Serial.println("Reconnected to Zebra scanner!");
+    tft.fillRect(0, 10, 320, 16, ILI9341_BLACK);
+    tft.setCursor(10, 10);
+    tft.setTextColor(ILI9341_GREEN);
+    tft.print("KASSEN : CONNECTED");
+  }
+  else
+  {
+    tft.fillRect(0, 10, 320, 16, ILI9341_BLACK);
+    tft.setCursor(10, 10);
+    tft.setTextColor(ILI9341_RED);
+    tft.print("KASSEN : DISCONNECTED");
+    Serial.println("Connectivity Status (): Bluetooth not connected, trying to reconnect...");
+  }
+  
+  // mqtt status
+  if(!mqttClient.connected())
+  {
+    tft.fillRect(0, 40, 320, 20, ILI9341_BLACK);
+    tft.setCursor(10, 40);
+    tft.setTextColor(ILI9341_RED);
+    tft.print("MQTT: NOT CONNECTED");
+  }
+  else
+  {
+    tft.fillRect(0, 40, 320, 20, ILI9341_BLACK);
+    tft.setCursor(10, 40);
+    tft.setTextColor(ILI9341_GREEN);
+    tft.print("MQTT: CONNECTED");
   }
 }
 void buttonRoutine()
 { 
+  //clearing buffer
   if(digitalRead(BUTTON2_PIN)==LOW){
     if (!button2WasPressed){
       rfidReadResult="";
       barcodeReadResult="";
-      rfidShowTFT();
-      barcodeShowTFT();
+      tftShowBarcode();
+      tftShowRfid();
     }
     button2WasPressed=true;
   }else{
@@ -326,17 +295,22 @@ void buttonRoutine()
         serializeJson(doc, jsonString);
         Serial.println("Sending data to MQTT: " + jsonString);
         if (mqttClient.connected())
-        {
-          mqttClient.publish("mkm/up/scanner/rfid/telemetry", jsonString.c_str());
-          tft.fillRect(0, 120, 320, 20, ILI9341_BLACK);
-          tft.setCursor(10, 120);
+        { 
+          mqttClient.publish(telemetryTopic, jsonString.c_str());
+          tft.fillRect(0,100, 320, 20, ILI9341_BLACK);
+          tft.setCursor(10,100);
           tft.setTextColor(ILI9341_GREEN);
           tft.print("Data sent to MQTT");
+          barcodeReadResult="";
+          rfidReadResult="";
+          tftShowBarcode();
+          tftShowRfid();
+
         }
         else
         {
-          tft.fillRect(0, 120, 320, 20, ILI9341_BLACK);
-          tft.setCursor(10, 120);
+          tft.fillRect(0, 100, 320, 20, ILI9341_BLACK);
+          tft.setCursor(10, 100);
           tft.setTextColor(ILI9341_RED);
           tft.print("MQTT not connected");
         }
@@ -349,59 +323,6 @@ void buttonRoutine()
   }
 }
 
-void ethCheckLink()
-{
-  tft.setCursor(10, 10);
-  if (Ethernet.linkStatus() == LinkOFF)
-  {
-    tft.println("Ethernet: FAIL");
-    Serial.println("⚠️ Ethernet cable not connected!");
-  }
-  else
-  {
-    tft.println("Ethernet: OK");
-    Serial.println("✅ Ethernet connected!");
-  }
-}
-void RFIDRoutine(void *parameter)
-{
-  while (1)
-  {
-    rfidRead();
-
-    vTaskDelay(100 / portTICK_PERIOD_MS); // Adjust delay as needed
-  }
-}
-void OTAUpdate(void *parameter)
-{
-  while (1)
-  {
-    ArduinoOTA.handle();
-    vTaskDelay(100/ portTICK_PERIOD_MS); // Adjust delay as needed
-  }
-}
-void MainLoop(void *parameter)
-{
-  while (1)
-  {
-   
-    buttonRoutine();
-    barcodeMantain();
-    barcodeRead();
-
-    if (mqttClient.connected())
-    {
-      heartBeatRoutine();
-      
-    }else{
-      connectToMQTT();
-    }
-    
-
-    mqttClient.loop();
-    vTaskDelay(100 / portTICK_PERIOD_MS); // Adjust delay as needed
-  }
-}
 void setup()
 {
   Serial.begin(115200);
@@ -411,7 +332,13 @@ void setup()
   Serial.println("TFT Begin");
   tft.begin();
 
-  // === Ethernet INIT ===
+  // === Ethernet INIT ===2
+  delay(1000);
+  pinMode(ETH_RST, OUTPUT);
+  digitalWrite(ETH_RST, LOW); // Reset Ethernet module
+  delay(2000);
+  digitalWrite(ETH_RST, HIGH); // Release reset
+  delay(1000);
   Serial.println("ETH init");
   Ethernet.init(ETH_SPI_SCS);
   Ethernet.begin(mac, ip, gateway, subnet);
@@ -422,14 +349,16 @@ void setup()
 
   // === ETHERNET STATUS ===
   Serial.println("Ethernet check");
-  ethCheckLink();
+
   // === RFID INIT ===
   Serial.println("Rfid init ");
   rfidSetup();
   // === OTA INIT ===
   Serial.println("Ota init");
   ArduinoOTA.begin(Ethernet.localIP(), "arduino", "password", InternalStorage);
- 
+  // === Bluetooth INIT ===
+  Serial.println("Barcode begin");
+  barcodeBegin();
   // === MQTT INIT ===
   Serial.println("MQTT Init");
   mqttClient.setServer(mqttServer, MQTT_Port);
@@ -438,42 +367,35 @@ void setup()
   // === BUTTON INIT ===
   Serial.println("button init ");
   buttonInit();
-   // === Bluetooth INIT ===
-  Serial.println("Barcode begin");
-  barcodeBegin();
+  
    // RTOS init 
-  Serial.println("RTOS INIT ");
-   xTaskCreatePinnedToCore(
-      MainLoop,   // Function to implement the task
-      "MainLoop", // Name of the task
-      10000,      // Stack size in words
-      NULL,       // Task input parameter
-      1,          // Priority of the task
-      NULL,       // Task handle
-      1);     
-  // === Create Tasks ===
-  xTaskCreatePinnedToCore(
-      OTAUpdate,   // Function to implement the task
-      "OTAUpdate", // Name of the task
-      10000,       // Stack size in words
-      NULL,        // Task input parameter
-      1,           // Priority of the task
-      NULL,        // Task handle
-      1);          // Core where the task should run (0 or 1)
-     // Core where the task should run (0 or 1)
-  xTaskCreatePinnedToCore(
-      RFIDRoutine,   // Function to implement the task
-      "RFIDRoutine", // Name of the task
-      10000,         // Stack size in words
-      NULL,          // Task input parameter
-      2,             // Priority of the task
-      NULL,          // Task handle
-      1);            // Core where the task should run (0 or 1)
-   
-
 }
 
 void loop()
 {
   
+  if (mqttClient.connected())
+  {
+    if( (nowT+500) <= millis())
+    {
+      
+      ConnectivityStatusTFT();
+      barcodeRead();
+      rfidRead();
+      barcodeMantain();
+      nowT = millis();
+      
+    }
+    heartBeatRoutine();
+    buttonRoutine();
+    
+    
+  }
+  else{
+    connectToMQTT();
+  }
+  ArduinoOTA.handle();
+
+  mqttClient.loop();
+  delay(10); // Small delay to avoid blocking the loop
 }
